dace28a2bfe997c0aa155a7bc961b557dad14394
wolfGhost23 <crvargasm@unal.edu.co>
2022-06-14 03:16:57 -0500

    nav fixed

--- ejercicioClase/posesion/gestionPosesion.php
+++ ejercicioClase/posesion/gestionPosesion.php
@@ -103,7 +103,7 @@ require "../database.php";
                             }
                             ?>
                         </select>
-                        <a href="../vivienda/getionVivienda.php">
+                        <a href="../vivienda/gestionVivienda.php">
                             <button class="btn btn-outline-secondary" type="button" id="button-addon1">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
                                     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
